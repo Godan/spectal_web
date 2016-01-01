c0d0e2cbeb18a7f473584c3c4630aa7a280e2306
root <shogo070920@gmail.com>
2016-01-01 23:56:20 +0900

    rename

--- web_site/application/views/top.php
+++ web_site/application/views/top.php
@@ -1,57 +1,55 @@
-
 <!--更新履歴-->
 <section class="container">
-	<div class="row">
-		<div class="col-md-8" style="background-color:#000000" >
-			<img src="<?=base_url();?>img/top-01.JPG">
-			</div>
-				<div class="list-group col-md4">
-					<a href="#" class="list-group-item active">New Topics!</a>
+    <div class="row">
+        <div class="col-md-8" style="background-color:#000000">
+            <img src="<?=base_url();?>images/top-01.JPG">
+        </div>
+        <div class="list-group col-md-4">
+            <a href="#" class="list-group-item active">New Topics!</a>
 
 
-					<a href="index.html" class="list-group-item"><span class="label label-success">新規</span>9/6
-						コンテンツにFE試験日まで何日カウンターを公開しました</a>
-					<a href="nyubu.html" class="list-group-item"><span class="label label-info">更新</span> 8/28
-						相談フォーム正式実装しました。　</a>
-					<a href="nyubu.html" class="list-group-item"><span class="label label-info">更新</span> 8/25
-						入部フォーム正式実装しました。　</a>
-					<a href="#" class="list-group-item"><span class="label label-success">新規</span> 8/8
-						幾つか修正、学内活動に関してを追加。</a>
-					<a href="#" class="list-group-item"><span class="label label-success">新規</span> 8/7　About
-						us、活動実績、入部希望者へ（未実装）をページ追加しました。　</a>
-					<a href="Link.html" class="list-group-item"><span class="label label-info">更新</span>
-						8/6　リンクにConoHa、岩崎学園グループを追加しました。　</a>
-		</div>
-		</div>
-	</section>
+            <a href="index.html" class="list-group-item"><span class="label label-success">新規</span>9/6
+                コンテンツにFE試験日まで何日カウンターを公開しました</a>
+            <a href="nyubu.html" class="list-group-item"><span class="label label-info">更新</span> 8/28
+                相談フォーム正式実装しました。　</a>
+            <a href="nyubu.html" class="list-group-item"><span class="label label-info">更新</span> 8/25
+                入部フォーム正式実装しました。　</a>
+            <a href="#" class="list-group-item"><span class="label label-success">新規</span> 8/8
+                幾つか修正、学内活動に関してを追加。</a>
+            <a href="#" class="list-group-item"><span class="label label-success">新規</span> 8/7　About
+                us、活動実績、入部希望者へ（未実装）をページ追加しました。　</a>
+            <a href="Link.html" class="list-group-item"><span class="label label-info">更新</span>
+                8/6　リンクにConoHa、岩崎学園グループを追加しました。　</a>
+        </div>
+    </div>
+</section>
 
 
+<!--ここからフッダー-->
+<div class="page-header" class="container col-md-12">
+    <div class="text-center">
+        <h5>横浜医療情報専門学校プログラミングサークル
+            <small>
+                <address><p>〒222－0033　神奈川県横浜市港北区新横浜2-4-10</address>
+                <a href="http://www.conoha.jp/" target="_blank">Powered by ConoHa</a></p>
+            </small>
+        </h5>
 
-	<!--ここからフッダー-->
-	<div class="page-header" class="container col-md-12" >
-	<div class="text-center">
-		<h5>横浜医療情報専門学校プログラミングサークル
-			<small>
-				<address><p>〒222－0033　神奈川県横浜市港北区新横浜2-4-10</address>
-				<a href="http://www.conoha.jp/" target="_blank">Powered by ConoHa</a></p>
-			</small>
-		</h5>
+        <ul class="breadcrumb" 　>
+            <li><a href="index.html">Home</a></li>
+            <li><a href="aboutus.html">About us</a></li>
+            <li><a href="Activity.html">活動実績</a></li>
+            <li><a href="blog.html">ブログ</a></li>
+            <li><a href="content.html">コンテンツ</a></li>
+            <li><a href="Link.html">リンク</a></li>
+        </ul>
+        <h6>
+            COPYRIGHT © IWASAKI GAKUEN Programming Group. ALL RIGHTS RESERVED.</h6>
+    </div>
+</div>
 
-		<ul class="breadcrumb" 　>
-			<li><a href="index.html">Home</a></li>
-			<li><a href="aboutus.html">About us</a></li>
-			<li><a href="Activity.html">活動実績</a></li>
-			<li><a href="blog.html">ブログ</a></li>
-			<li><a href="content.html">コンテンツ</a></li>
-			<li><a href="Link.html">リンク</a></li>
-		</ul>
-		<h6>
-			COPYRIGHT © IWASAKI GAKUEN Programming Group. ALL RIGHTS RESERVED.</h6>
-	</div>
-	</div>
-
-	<!--Loding jQuery -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<!--Loding useJavaScript-->
-	<script src="js/bootstrap.min.js"></script>
-	</body>
+<!--Loding jQuery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<!--Loding useJavaScript-->
+<script src="js/bootstrap.min.js"></script>
+</body>
