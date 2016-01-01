0e9d819f24e3d2ca06b6e3c87fbff6a31e620880
root <shogo070920@gmail.com>
2016-01-01 23:02:04 +0900

    fix

--- web_site/application/controllers/Top.php
+++ web_site/application/controllers/Top.php
@@ -1,9 +1,25 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Top extends CI_Controller
+class Top extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+* @see http://codeigniter.com/user_guide/general/urls.html
+*/
+public function show()
 {
-
-	public function show(){
-		$this->load->view('heder');
+$this->load->view('welcome_message');
 }
 }
